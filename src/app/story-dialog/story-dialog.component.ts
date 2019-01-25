2fc333bd2eebcf1f7fbcab206b42a7a534514d2c
Tobias <tobi-krueger@t-online.de>
2019-01-25 23:29:47 +0100

    added Material module, story service and storydialog component

---
+++ src/app/story-dialog/story-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-story-dialog',
+  templateUrl: './story-dialog.component.html',
+  styleUrls: ['./story-dialog.component.scss']
+})
+export class StoryDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
