9973c245809e1ca5621c211b41634e8cd99d3718
Theo <PeThHa>
2019-01-25 23:37:38 +0100

    stylish buttons

--- src/app/level/agb/agb.component.ts
+++ src/app/level/agb/agb.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agb',
@@ -12,7 +14,8 @@ export class AgbComponent implements OnInit {
 
   private disableAccept: boolean = true;
 
-  constructor() { }
+  constructor(private news: NewsService,
+              private router: Router) { }
 
   ngOnInit() {
     this.startTimer();
@@ -32,53 +35,52 @@ export class AgbComponent implements OnInit {
   }
 
   pauseTimer() {
-    console.log(this.time);
     clearInterval(this.interval);
   }
 
   evalTime() {
     switch(true) {
       case (this.time < 2): {
-        console.log("You broke the sound barrier!");
+        this.news.showErrorMsg("","You broke the sound barrier!");
         break;
       }
       case (this.time < 5): {
-        console.log("Fastest scroller in the West!");
+        this.news.showErrorMsg("","Fastest scroller in the West!");
         break;
       }
-      case (this.time < 10): {
-        console.log("Don't tell me you read everything! Did your parents not tell you not to lie?!?");
+      case (this.time < 8): {
+        this.news.showErrorMsg("","Don't tell me you read everything! Did your parents not tell you not to lie?!?");
         break;
       }
-      case (this.time < 15): {
-        console.log("Guess you don't care about the details.");
+      case (this.time < 11): {
+        this.news.showErrorMsg("","Guess you don't care about the details.");
+        break;
+      }
+      case (this.time < 14): {
+        this.news.showErrorMsg("","Sure you didn't miss anything?");
+        break;
+      }
+      case (this.time < 17): {
+        this.news.showErrorMsg("","We're getting there.");
         break;
       }
       case (this.time < 20): {
-        console.log("Sure you didn't miss anything?");
+        this.news.showErrorMsg("","Did you not forget the small print?");
         break;
       }
-      case (this.time < 25): {
-        console.log("We're getting there.");
+      case (this.time < 23): {
+        this.news.showErrorMsg("","Your decision could heavily affect your future. Take some seconds to think about it.");
         break;
       }
-      case (this.time < 30): {
-        console.log("Did you not forget the small print?");
-        break;
-      }
-      case (this.time < 35): {
-        console.log("Your decision could heavily affect your future. Take some seconds to think about it.");
-        break;
-      }
-      case (this.time < 40): {
-        console.log("Maybe we're selling all of your data or microwaving butterflies in our spare time. You really should read carefully.");
+      case (this.time < 26): {
+        this.news.showErrorMsg("","Maybe we're selling all of your data or microwaving butterflies in our spare time. You really should read carefully.");
         break;
       }
       default: {
-        console.log("You made your choice. Now go and live with it.");
+        this.news.showErrorMsg("","You made your choice. Now go and live with it.");
+        this.router.navigate(['home'])
         break;
       }
     }
-    // TODO Link aufruf einfügen
   }
 }
