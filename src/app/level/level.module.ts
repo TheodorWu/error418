8285b4372111b41e8009cb8a6be376a74e2b1eea
Theo <PeThHa>
2019-01-26 03:20:38 +0100

    lampe geht so halb

--- src/app/level/level.module.ts
+++ src/app/level/level.module.ts
@@ -4,11 +4,13 @@ import { AgbComponent } from './agb/agb.component';
 import { LoadingComponent } from './loading/loading.component';
 import { LanguageComponent } from './language/language.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [AgbComponent, LoadingComponent, LanguageComponent, NotfoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
   ],
   exports:
   [AgbComponent],
--- src/app/level/notfound/notfound.component.ts
+++ src/app/level/notfound/notfound.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-notfound',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notfound.component.scss']
 })
 export class NotfoundComponent implements OnInit {
+  gradientTop: number;
+  gradientLeft: number;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e) {
+    console.log(e);
+    // let mouseXpercentage = Math.round(e.X / windowWidth * 100);
+    // let mouseYpercentage = Math.round(e.Y / windowHeight * 100);
+
+    let elem = document.getElementById('gradient');
+    console.log(e.clientX,e.clientWidth,e.clientY,e.clientHeight)
+    elem.setAttribute('style', 'background: radial-gradient(50px at ' + e.clientX/e.target.clientWidth*100 + '% ' + e.clientY/e.target.clientHeight*100 + '%, white, black);');
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // $(document).mousemove(function(event) {
+  //   windowWidth = $(window).width();
+  //   windowHeight = $(window).height();
+  //
+  //   mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+  //   mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+  //
+  //   $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
+  // });
 }
