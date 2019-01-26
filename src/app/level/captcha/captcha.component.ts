9e6fbbb4f52f23299879bc889e8e886a38dc6cc1
Tobias <tobi-krueger@t-online.de>
2019-01-26 13:22:35 +0100

    now only two captcha cards can be turned at a time

--- src/app/level/captcha/captcha.component.ts
+++ src/app/level/captcha/captcha.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CaptchaModel, CAPTCHAS, CaptchaAsset } from './captcha.model';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-captcha',
@@ -9,9 +10,10 @@ import { CaptchaModel, CAPTCHAS, CaptchaAsset } from './captcha.model';
 export class CaptchaComponent implements OnInit {
 
   currentCaptcha: CaptchaModel;
-  currentlyRevealed = [];
+  currentlyRevealed: Array<CaptchaAsset> = [];
 
-  constructor() { }
+  constructor(private errorService: NewsService) {
+  }
 
   ngOnInit() {
     this.currentCaptcha = CAPTCHAS[0];
@@ -24,18 +26,27 @@ export class CaptchaComponent implements OnInit {
   }
 
   flip(captcha: CaptchaAsset, index: number) {
-    if (captcha.flipped) {
-      document.getElementById('Card' + index).setAttribute('style', 'transform: rotateY( 0deg );');
-      const idx = this.currentlyRevealed.indexOf(captcha);
-      if (idx > -1) {
-        this.currentlyRevealed.splice(idx, 1);
+      if (captcha.flipped) {
+        document.getElementById('Card' + index).setAttribute('style', 'transform: rotateY( 0deg );');
+        const idx = this.currentlyRevealed.indexOf(captcha);
+        if (idx > -1) {
+          this.currentlyRevealed.splice(idx, 1);
+        }
+      } else {
+        if (this.currentlyRevealed.length > 2) {
+          this.errorService.showErrorMsg('Action not Allowed', 'You have already revealed two Cards');
+        } else {
+          document.getElementById('Card' + index).setAttribute('style', 'transform: rotateY( 180deg );');
+          this.currentlyRevealed.push(captcha);
+          if (this.currentlyRevealed.length === 2) {
+            if (this.currentlyRevealed[0].id === this.currentlyRevealed[1].id) {
+              this.currentlyRevealed = [];
+            }
+          }
+        }
       }
-    } else {
-      document.getElementById('Card' + index).setAttribute('style', 'transform: rotateY( 180deg );');
-      this.currentlyRevealed.push(captcha);
-    }
-    captcha.flipped = !captcha.flipped;
-    console.log(this.currentlyRevealed);
+      captcha.flipped = !captcha.flipped;
+      console.log(this.currentlyRevealed);
 
   }
 
