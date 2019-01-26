b9c52e70845bfb45caa3a5f886018de21a30c29e
Tobias <tobi-krueger@t-online.de>
2019-01-26 19:36:02 +0100

    added basic styling for teapot boss fight

--- src/assets/texts/StoryLine.ts
+++ src/assets/texts/StoryLine.ts
@@ -5,7 +5,7 @@ export const STORY_TXT = [
   {num: 3, content: 'Well that was intense, let\'s see what all that loading was for', next: ['/captcha']},
   // tslint:disable-next-line:max-line-length
   {num: 4, content: 'Why do Captcha\'s always use car pictures?? Well atleast I didn\'t need to download anything yet. I hate how difficult it always is to find the right Button', next: ['/downloads']},
-
+  {num: 5, content: 'ERROR 418 I\'m a Teapot', next: ['/teapot']},
   // Should always be the last element
   {num: 6, content: 'You Have done it ... so cool ...', next: ['/home']}
 ];
