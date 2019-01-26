5670f8c7bc992d86bb929600fa4927bb9bd4162d
Theo <PeThHa>
2019-01-26 01:01:26 +0100

    neue componenten

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AgbComponent } from './level/agb/agb.component';
+import { NotfoundComponent } from './level/notfound/notfound.component';
 
 const routes: Routes = [
   { path: 'home', component: AppComponent },
   { path: 'agb', component: AgbComponent },
+  { path: '404', component: NotfoundComponent }
 ];
 
 @NgModule({
