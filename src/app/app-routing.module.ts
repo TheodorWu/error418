05fccda355dc82db6f6d2b786846cf02ccaab8c5
Tobias <tobi-krueger@t-online.de>
2019-01-26 02:08:09 +0100

    added hannes loading game code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AgbComponent } from './level/agb/agb.component';
 import { NotfoundComponent } from './level/notfound/notfound.component';
+import { LoadingComponent } from './level/loading/loading.component';
 
 const routes: Routes = [
   { path: 'home', component: AppComponent },
   { path: 'agb', component: AgbComponent },
-  { path: '404', component: NotfoundComponent }
+  { path: '404', component: NotfoundComponent },
+  { path: 'loading', component: LoadingComponent }
 ];
 
 @NgModule({
