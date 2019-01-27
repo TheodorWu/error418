9996080bf825fd1c0a0c2a173febb3fdd9f7d4a0
Theo <PeThHa>
2019-01-27 13:51:32 +0100

    tobis rechtschreibung angepasst

--- src/assets/texts/News.ts
+++ src/assets/texts/News.ts
@@ -1,20 +1,20 @@
 export const NEWS_TXT = [
   {
-    headline: 'New AI is released to improve the Internet',
+    headline: 'New AI is released to improve the internet',
     subheadline: 'Scientists say there is nothing to worry about'
   },
   {
-    headline: 'AI starts manipulating the Internet',
+    headline: 'AI starts manipulating the internet',
     subheadline:
-      'Apparently the AI has decided to make the Internet more "Fun" for Humans'
+      'Apparently the AI has decided to make the internet more "fun" for humans'
   },
   { headline: 'Scientists start to worry', subheadline: 'AI seems to have developed its own consciousness' },
-  { headline: 'Have You Tried turning it on and off again ?', subheadline: 'IT Specialist offers possible Solution to the AI Problem' },
-  { headline: 'AI takes over major Websites', subheadline: 'Replacing often ignored information and settings with small minigames' },
+  { headline: 'Have you tried turning it on and off again ?', subheadline: 'IT specialist offers possible solution to the AI problem' },
+  { headline: 'AI takes over major websites', subheadline: 'Replacing often ignored information and settings with small minigames' },
   {
     headline: 'ASU President suggestes building a wall around the Internet',
-    subheadline: 'Scientists are unsure wether that would solve the Problem'
+    subheadline: 'Scientists are unsure wether that would solve the problem'
   },
-  { headline: 'AI finally proves P unequal NP', subheadline: 'Scientists amazed by how easy the Prove was'},
-  { headline: 'AI takes over private Coffee Machines', subheadline: 'Scientists recommend switching to Tea Machines not supporting HTCPCP' }
+  { headline: 'AI finally proves P unequal NP', subheadline: 'Scientists amazed by how easy the prove was'},
+  { headline: 'AI takes over private coffee machines', subheadline: 'Scientists recommend switching to tea machines not supporting HTCPCP' }
 ];
